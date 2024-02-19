d61c1645809f34434dd5543cc1a9688a37cda479
Argha Das <hdark6336@gmail.com>
2024-02-20 00:35:05 +0530

    changes

--- userbalance.py
+++ userbalance.py
@@ -1,6 +1,16 @@
 from mongoengine import Document, StringField, DateTimeField, FloatField, BooleanField
-
+from pydantic import BaseModel
 class UserBalance(Document):
     user_id = StringField(required=True, max_length=200)
     balance = FloatField(required=True, max_length=200)
     status = BooleanField(required=True, default=True)
+
+
+class UserBalanceJsonModel(BaseModel):
+    user_id:str
+    balance: str
+
+class UserWithdrawalReq(BaseModel):
+    userid: str
+    name: str
+    amount: float
